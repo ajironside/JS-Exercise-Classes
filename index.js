da055167e458a8f2c74d43ed311ca134e3e57b3f
Amanda Ironside <ironside.amanda@gmail.com>
2020-01-30 13:07:04 -0700

    car function mostly complete, and Lambdasian function complete

--- index.js
+++ index.js
@@ -76,7 +76,24 @@ class Person {
 */
 
 class Car {
-
+    constructor(model, milesPerGallon) {
+        this.model = model;
+        this.milesPerGallon = milesPerGallon;
+        this.tank = 0;
+        this.odometer = 0;
+    }
+    fill(gallons) {
+        this.tank = this.tank + gallons;
+    }
+    drive(distance) {
+        this.odometer = this.odometer + distance;
+        this.distance = distance / this.milesPerGallon;
+        Math.round(this.distance);
+        this.tank = this.tank - this.distance;
+        if (this.tank <= 0) {
+            return `I ran out of fuel at ${this.odometer} miles!`;
+        }
+    }
 }
 
 /*
@@ -92,7 +109,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+    }
+    speak(name, location) {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
 }
 
 /*
